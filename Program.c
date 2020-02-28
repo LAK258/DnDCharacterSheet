446357b24b9197c82b823988df033e49e780add3
Solmand <solmands@gmail.com>
2020-02-28 13:10:50 +0100

    added second line

--- Program.c
+++ Program.c
@@ -6,7 +6,7 @@ namespace op1L2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Bitches!");
         }
     }
 }
