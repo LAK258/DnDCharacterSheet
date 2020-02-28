1dbf1b7d58b281f74ff554cde700e22f1b019b34
Lasse <Lasse54.kristensen@live.dk>
2020-02-28 13:08:47 +0100

    new shit
    hello

---
+++ Program.c
@@ -0,0 +1,12 @@
+using System;
+
+namespace op1L2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
