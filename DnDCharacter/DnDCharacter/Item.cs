b03265bbfb63f2f47e74afc588e9e087662a0ba2
Unknown <Lasse54.kristensen@live.dk>
2020-04-01 15:50:34 +0200

    sletter program
    dfhdh

--- DnDCharacter/DnDCharacter/Item.cs
+++ DnDCharacter/DnDCharacter/Item.cs
@@ -4,7 +4,58 @@ using System.Text;
 
 namespace DnDCharacter
 {
-    class Item
+     public class Item
     {
+        private string itemName;
+        private string itemType;
+        private int amountHeld;
+        private int weightPerItem;
+        private string description;
+
+        public Item()
+        {
+        }
+
+        public Item(string itemName)
+        {
+            this.itemName = itemName;
+        }
+
+        public Item(string itemName, string itemType, int amountHeld, int weightPerItem, string description)
+        {
+            this.itemName = itemName;
+            this.itemType = itemType;
+            this.amountHeld = amountHeld;
+            this.weightPerItem = weightPerItem;
+            this.description = description;
+        }
+
+        public string ItemName
+        {
+            get { return itemName; }
+            set { itemName = value; }
+        }
+        public string ItemType
+        {
+            get { return itemType; }
+            set { itemType = value; }
+        }
+        public int AmountHeld
+        {
+            get { return amountHeld; }
+            set { amountHeld = value; }
+        }
+        public int WeightPerItem
+        {
+            get { return weightPerItem; }
+            set { weightPerItem = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
     }
 }
